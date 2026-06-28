94f7bd7d02850ce9ab0718560ca9253af435f729
Aryan Dabholkar <aryandabholkar@Aryans-MacBook-Air.local>
2026-06-28 12:48:29 +0530

    Prepare project for Render Deployment

--- config.py
+++ config.py
@@ -3,16 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-
-    SECRET_KEY = os.getenv("SECRET_KEY")
-
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{os.getenv('DB_USERNAME')}:"
-        f"{os.getenv('DB_PASSWORD')}@"
-        f"{os.getenv('DB_HOST')}/"
-        f"{os.getenv('DB_NAME')}"
-    )
-
+    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "users.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
